61ec1cb9b6c228c2cd0506f6c4ebd60e134528fb
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-12 14:30:33 +0900

    break문

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -77,6 +77,20 @@ namespace csloop
                 }
                 Console.WriteLine("\n");
             }
+            //
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요, 짝수를 입력하면 종료: ");
+                int input = int.Parse(Console.ReadLine());
+                if (input % 2 == 0)
+                {
+                    break;
+                }
+            }
+
+            //
+
+
         }
     }
 }
