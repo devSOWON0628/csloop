ca9ea4b6f2d48fcb1568db9f1f15a1e0ec68ac93
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-12 14:31:45 +0900

    continue문

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -90,7 +90,14 @@ namespace csloop
 
             //
 
-
+           for(int i = 1; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
         }
     }
 }
